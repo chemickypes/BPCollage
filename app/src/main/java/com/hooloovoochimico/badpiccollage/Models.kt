199258046c38bb29d3263c2119d90411ed9fe0c6
Moroni Angelo <angelo.moroni@hype.it>
2019-08-03 11:55:13 +0200

    model of actions

--- app/src/main/java/com/hooloovoochimico/badpiccollage/Models.kt
+++ app/src/main/java/com/hooloovoochimico/badpiccollage/Models.kt
@@ -2,7 +2,9 @@ package com.hooloovoochimico.badpiccollage
 
 
 enum class ActionModelsEnum{
-    PICK_FROM_CAMERA, PICK_FROM_GALLERY, ADD_TEXT
+    PICK_FROM_CAMERA, PICK_FROM_GALLERY, ADD_TEXT,
+    INCREASE_TEXT,DECREASE_TEXT,CHOOSE_BG_TEXT_COLOR, CHOOSE_TEXT_COLOR,
+    CHOOSE_TEXT_FONT,CANCEL,EDIT_TEXT
 }
 
 data class ActionModels(val action:ActionModelsEnum)
