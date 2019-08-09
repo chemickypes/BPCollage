a147e3d22628db2e00b463d52f9c09545e60b6ba
Moroni Angelo <angelo.moroni@hype.it>
2019-08-09 12:22:19 +0200

    show browser

--- app/src/main/java/com/hooloovoochimico/badpiccollage/InfoActivity.kt
+++ app/src/main/java/com/hooloovoochimico/badpiccollage/InfoActivity.kt
@@ -1,12 +1,54 @@
 package com.hooloovoochimico.badpiccollage
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.manzo.slang.extensions.getAppVersion
+import com.manzo.slang.extensions.startActivity
+import kotlinx.android.synthetic.main.activity_info.*
 
 class InfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        val versionText = "v.${getAppVersion()}"
+        version_tv.text = versionText
+
+        buy_me_coffee_button.setOnClickListener {
+            showBrowser("https://ko-fi.com/angelomoroni")
+        }
+
+        credits_button.setOnClickListener {
+            showBrowser("https://github.com/chemickypes/BPCollage/blob/master/Credits.md")
+        }
+
+        github_button.setOnClickListener {
+            showBrowser("https://github.com/chemickypes/BPCollage")
+        }
+
+        license_button.setOnClickListener {
+            showBrowser("https://github.com/chemickypes/BPCollage/blob/master/LICENSE.md")
+        }
+
     }
+
+    fun showBrowser(url:String?) =
+        url?.let {
+            Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(url)
+            }.start(this)
+        }?: false
+
+
+}
+
+fun Intent.start(context: Context): Boolean {
+    return resolveActivity(context.packageManager)?.let {
+        context.startActivity(this)
+        true
+    } ?: false
 }
